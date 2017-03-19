cc5f15c6fe1e246caa2f7258ffbcf2c1ad5bc1ef
alexzhong22c <535377683@qq.com>
2017-03-19 23:14:10 +0800

    add-Bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import Hello from './components/Hello.vue'
 import Home from './components/Home.vue'
 // import router from './router'
 import store from './store'
@@ -10,20 +9,13 @@ import TimeEntries from './components/TimeEntries.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-
+import 'bootstrap/dist/css/bootstrap.css'
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   template: '<App/>',
-//   components: { App }
-// })
-
 const routes = [{
   path: '/',
   component: Home
